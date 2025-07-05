fdd5135ab534fb9807331d3a3155ea61350fdac7
yashdeep roy <yashdeeproy2004@gmail.com>
2025-07-05 22:50:44 +0530

    fix axios url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "https://api.example.com";
+const baseURL = import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api";
 const api = axios.create({
     baseURL: baseURL,
     headers: {
